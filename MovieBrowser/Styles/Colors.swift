791e98dee15fa84228f252a3aadc13f60a354ba2
Gheorghe Soltan <gheorghe.soltan@think-it.ro>
2019-11-06 11:40:07 +0200

    added app colors

---
+++ MovieBrowser/Styles/Colors.swift
@@ -0,0 +1,18 @@
+//
+// Created by soltan on 06/11/2019.
+// Copyright (c) 2019 personal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let primaryColor = UIColor(red: 0.88, green: 0.25, blue: 0.98, alpha: 1.0);
+    static let primaryLightColor = UIColor(red: 1.00, green: 0.47, blue: 1.00, alpha: 1.0);
+    static let primaryDarkColor = UIColor(red: 0.67, green: 0.00, blue: 0.78, alpha: 1.0);
+    static let primaryTextColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.0);
+    static let secondaryColor = UIColor(red: 0.87, green: 0.17, blue: 0.00, alpha: 1.0);
+    static let secondaryLightColor = UIColor(red: 1.00, green: 0.39, blue: 0.20, alpha: 1.0);
+    static let secondaryDarkColor = UIColor(red: 0.64, green: 0.00, blue: 0.00, alpha: 1.0);
+    static let secondaryTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+}
