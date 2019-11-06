38d3018a61f1d14f18f32e720592c126df93df46
Gheorghe Soltan <gheorghe.soltan@think-it.ro>
2019-11-06 15:05:27 +0200

    converted to workspace, added cocoapods

--- MovieBrowser/Styles/Colors.swift
+++ MovieBrowser/Styles/Colors.swift
@@ -5,25 +5,62 @@
 
 import Foundation
 import UIKit
+import MaterialComponents
 
 protocol AppColors {
-    static var primaryColor : UIColor { get }
-    static var primaryLightColor : UIColor { get }
-    static var primaryDarkColor : UIColor { get }
-    static var primaryTextColor : UIColor { get }
-    static var secondaryColor : UIColor { get }
-    static var secondaryLightColor : UIColor { get }
-    static var secondaryDarkColor : UIColor { get }
-    static var secondaryTextColor : UIColor { get }
+    var primaryColor : UIColor { get }
+    var primaryLightColor : UIColor { get }
+    var primaryDarkColor : UIColor { get }
+    var primaryTextColor : UIColor { get }
+    var secondaryColor : UIColor { get }
+    var secondaryLightColor : UIColor { get }
+    var secondaryDarkColor : UIColor { get }
+    var secondaryTextColor : UIColor { get }
 }
 
-extension UIColor : AppColors {
-    static let primaryColor = UIColor(red: 0.88, green: 0.25, blue: 0.98, alpha: 1.0);
-    static let primaryLightColor = UIColor(red: 1.00, green: 0.47, blue: 1.00, alpha: 1.0);
-    static let primaryDarkColor = UIColor(red: 0.67, green: 0.00, blue: 0.78, alpha: 1.0);
-    static let primaryTextColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.0);
-    static let secondaryColor = UIColor(red: 0.87, green: 0.17, blue: 0.00, alpha: 1.0);
-    static let secondaryLightColor = UIColor(red: 1.00, green: 0.39, blue: 0.20, alpha: 1.0);
-    static let secondaryDarkColor = UIColor(red: 0.64, green: 0.00, blue: 0.00, alpha: 1.0);
-    static let secondaryTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+protocol AppFonts {
+    var mainFont : UIFont { get }
+}
+
+protocol AppStyle : class, AppColors, AppFonts {
+
+}
+
+class MainStyle : AppStyle {
+
+
+    //MARK colors
+    let primaryColor = UIColor(red: 0.88, green: 0.25, blue: 0.98, alpha: 1.0);
+    let primaryLightColor = UIColor(red: 1.00, green: 0.47, blue: 1.00, alpha: 1.0);
+    let primaryDarkColor = UIColor(red: 0.67, green: 0.00, blue: 0.78, alpha: 1.0);
+    let primaryTextColor = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha: 1.0);
+    let secondaryColor = UIColor(red: 0.87, green: 0.17, blue: 0.00, alpha: 1.0);
+    let secondaryLightColor = UIColor(red: 1.00, green: 0.39, blue: 0.20, alpha: 1.0);
+    let secondaryDarkColor = UIColor(red: 0.64, green: 0.00, blue: 0.00, alpha: 1.0);
+    let secondaryTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+
+
+    //MARK fonts
+    let mainFont: UIFont = UIFont(name: "ChalkboardSE-Bold", size: UIFont.systemFontSize)!
+
+}
+
+class StyleManager {
+    private init() {
+        currentStyle = MainStyle()
+    }
+    static let shared = StyleManager()
+    var currentStyle : AppStyle
+
+}
+
+extension UIColor  {
+    static let primaryColor = StyleManager.shared.currentStyle.primaryColor;
+    static let primaryLightColor = StyleManager.shared.currentStyle.primaryLightColor;
+    static let primaryDarkColor = StyleManager.shared.currentStyle.primaryTextColor;
+    static let primaryTextColor = StyleManager.shared.currentStyle.primaryTextColor;
+    static let secondaryColor = StyleManager.shared.currentStyle.primaryTextColor;
+    static let secondaryLightColor = StyleManager.shared.currentStyle.secondaryLightColor;
+    static let secondaryDarkColor = StyleManager.shared.currentStyle.secondaryDarkColor;
+    static let secondaryTextColor = StyleManager.shared.currentStyle.secondaryTextColor;
 }
