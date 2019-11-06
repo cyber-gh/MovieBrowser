2985101136d3a73eb1f7939da5bc4844cc17b535
Gheorghe Soltan <gheorghe.soltan@think-it.ro>
2019-11-06 11:17:43 +0200

    template for adding app colors

---
+++ MovieBrowser/Styles/Styles.swift
@@ -0,0 +1,11 @@
+//
+// Created by soltan on 06/11/2019.
+// Copyright (c) 2019 personal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var primaryColor : UIColor = .black
+}
