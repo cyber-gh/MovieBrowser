77628b7118549235c0cdc2d9caf86e39be876caf
Gheorghe Soltan <gheorghe.soltan@think-it.ro>
2019-11-14 17:00:09 +0200

    functional click listener

---
+++ MovieBrowser/Extensions/UIButton+ClickListener.swift
@@ -0,0 +1,52 @@
+//
+// Created by soltan on 14/11/2019.
+// Copyright (c) 2019 personal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+typealias UIButtonTargetClosure = (UIButton) -> ()
+
+class ClosureWrapper: NSObject {
+    let closure: UIButtonTargetClosure
+
+    init(_ closure: @escaping UIButtonTargetClosure) {
+        self.closure = closure
+    }
+}
+
+// pure Objective-C magic
+extension UIButton {
+
+    private struct AssociatedKeys {
+        static var targetClosure = "targetClosure"
+    }
+
+    private var targetClosure: UIButtonTargetClosure? {
+        get {
+            guard let closureWrapper = objc_getAssociatedObject(self, &AssociatedKeys.targetClosure) as? ClosureWrapper else {
+                return nil
+            }
+            return closureWrapper.closure
+        }
+        set(newValue) {
+            guard let newValue = newValue else {
+                return
+            }
+            objc_setAssociatedObject(self, &AssociatedKeys.targetClosure, ClosureWrapper(newValue), objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+
+    func addTargetClosure(closure: @escaping UIButtonTargetClosure) {
+        targetClosure = closure
+        addTarget(self, action: #selector(UIButton.closureAction), for: .touchUpInside)
+    }
+
+    func closureAction() {
+        guard let targetClosure = targetClosure else {
+            return
+        }
+        targetClosure(self)
+    }
+}
